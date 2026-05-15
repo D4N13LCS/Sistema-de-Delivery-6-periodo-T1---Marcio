1a7de1e0908f381e9e4c6b712137dad57394b683
D4N13LCS <danielmazzini6@gmail.com>
2026-05-15 15:56:33 -0300

    feat: implement product catalog module

---
+++ delivery_project/products/views.py
@@ -0,0 +1,16 @@
+from django.shortcuts import render
+from django.views.generic import TemplateView
+from products.models import Produto
+
+# Create your views here.
+class HomeView(TemplateView):
+
+    template_name = 'home.html'
+
+    def get_context_data(self, **kwargs):
+
+        context = super().get_context_data(**kwargs)
+
+        context['produtos'] = Produto.objects.all()
+
+        return context
