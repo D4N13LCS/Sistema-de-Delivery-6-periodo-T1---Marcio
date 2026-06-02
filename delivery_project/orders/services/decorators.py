7c4158fa004011fdd26e2ab010e07626f894e882
D4N13LCS <danielmazzini6@gmail.com>
2026-06-02 20:59:09 -0300

    refactor: improve decorator structure for burger extras

--- delivery_project/orders/services/decorators.py
+++ delivery_project/orders/services/decorators.py
@@ -1,9 +1,20 @@
-class Lanche:
+from abc import ABC, abstractmethod
+
+
+class ItemPedido(ABC):
+
+    @abstractmethod
+    def descricao(self):
+        pass
+
+    @abstractmethod
+    def preco(self):
+        pass
+
+class Lanche(ItemPedido):
 
     def __init__(self, nome, preco):
-
         self.nome = nome
-
         self.valor = preco
 
     def descricao(self):
@@ -11,42 +22,40 @@ class Lanche:
 
     def preco(self):
         return self.valor
+    
+class AdicionalDecorator(ItemPedido):
 
+    def __init__(self, item):
+        self.item = item
 
-class ExtraQueijo:
+class ExtraQueijo(AdicionalDecorator):
 
-    def __init__(self, lanche):
-
-        self.lanche = lanche
-
-    def descricao(self):
-        return self.lanche.descricao() + " + Queijo"
-
-    def preco(self):
-        return self.lanche.preco() + 5
-
-
-class Bacon:
-
-    def __init__(self, lanche):
-
-        self.lanche = lanche
+    PRECO = 5
 
     def descricao(self):
-        return self.lanche.descricao() + " + Bacon"
+        return f"{self.item.descricao()} + Queijo"
 
     def preco(self):
-        return self.lanche.preco() + 7
+        return self.item.preco() + self.PRECO
 
 
-class Catupiry:
+class Bacon(AdicionalDecorator):
 
-    def __init__(self, lanche):
-
-        self.lanche = lanche
+    PRECO = 7
 
     def descricao(self):
-        return self.lanche.descricao() + " + Catupiry"
+        return f"{self.item.descricao()} + Bacon"
 
     def preco(self):
-        return self.lanche.preco() + 6
+        return self.item.preco() + self.PRECO
+
+
+class Catupiry(AdicionalDecorator):
+
+    PRECO = 6
+
+    def descricao(self):
+        return f"{self.item.descricao()} + Catupiry"
+
+    def preco(self):
+        return self.item.preco() + self.PRECO
