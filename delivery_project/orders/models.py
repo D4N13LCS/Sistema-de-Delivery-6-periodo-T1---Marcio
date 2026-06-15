f66781f851388613d8ba84ccabd5718d10b37e86
D4N13LCS <danielmazzini6@gmail.com>
2026-06-15 01:56:40 -0300

    refactor(orders): update order model and history presentation

--- delivery_project/orders/models.py
+++ delivery_project/orders/models.py
@@ -1,39 +1,39 @@
-from django.db import models
-from django.contrib.auth.models import User
+# from django.db import models
+# from django.contrib.auth.models import User
 
-# Create your models here.
-class Pedido(models.Model):
+# # Create your models here.
+# class Pedido(models.Model):
 
-    usuario = models.ForeignKey(
-        User,
-        on_delete=models.CASCADE
-    )
+#     usuario = models.ForeignKey(
+#         User,
+#         on_delete=models.CASCADE
+#     )
 
-    produto_id = models.IntegerField(null=True)
+#     produto_id = models.IntegerField(null=True)
 
-    produto_nome = models.CharField(
-        max_length=100,
-        null=True,
-        blank=True
-    )
+#     produto_nome = models.CharField(
+#         max_length=100,
+#         null=True,
+#         blank=True
+#     )
 
-    produto_preco = models.FloatField(null=True)
+#     produto_preco = models.FloatField(null=True)
 
-    adicionais = models.TextField()
+#     adicionais = models.TextField()
 
-    entrega = models.CharField(max_length=30)
+#     entrega = models.CharField(max_length=30)
 
-    pagamento = models.CharField(max_length=30)
+#     pagamento = models.CharField(max_length=30)
 
-    subtotal = models.FloatField()
+#     subtotal = models.FloatField()
 
-    desconto = models.FloatField()
+#     desconto = models.FloatField()
 
-    taxa_entrega = models.FloatField()
+#     taxa_entrega = models.FloatField()
 
-    valor_total = models.FloatField()
+#     valor_total = models.FloatField()
 
-    criado_em = models.DateTimeField(auto_now_add=True)
+#     criado_em = models.DateTimeField(auto_now_add=True)
 
-    def __str__(self):
-        return f'Pedido #{self.id}'
+#     def __str__(self):
+#         return f'Pedido #{self.id}'
